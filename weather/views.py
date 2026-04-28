6dcd11b74e15b1cc1eb0d963b6e856175809d0aa
Antonio <antonio42480@gmail.com>
2026-04-28 00:53:01 -0600

    Fixed the London default city and logic

--- weather/views.py
+++ weather/views.py
@@ -1,34 +1,32 @@
-import requests # A library that lets Python "visit" websites (APIs)
-from django.shortcuts import render # Helps us "draw" the HTML on the screen
+import requests
+from django.shortcuts import render
 
-# Create your views here.
 def index(request):
-    # 1. Grab the city name from the search bar (the URL)
-    # If the user hasn't typed anything yet, default to 'London'
-    city = request.GET.get('city', 'London')
+    # 1. Start with an empty context (this keeps the screen blank at first)
+    context = {}
 
-    # 2. Setup our API info (We'll need a real key soon!)
-    api_key = 'f760f3e666e217eb086d68617411504f'
-    url = f'http://api.openweathermap.org/data/2.5/weather?q={city}&appid={api_key}&units=metric'
+    if request.method == 'POST':
+        city = request.POST.get('city')
+        api_key = 'f760f3e666e217eb086d68617411504f'
+        url = f'http://api.openweathermap.org/data/2.5/weather?q={city}&appid={api_key}&units=metric'
 
-    # 3. Go get the data from the internet
-    response = requests.get(url).json() # Convert the data to a Python dictionary
+        # 2. Only go to the internet if the user actually searched for something
+        response = requests.get(url).json()
 
-    # Logic Check: Did the API find the city? (Status code 200 means "OK")
-    if response.get('cod') == 200:
-        context = {
-            'city': response['name'],
-            'temp': response['main']['temp'],
-            'desc': response['weather'][0]['description'],
-            'icon': response['weather'][0]['icon'], # A code for the weather icon (e.g. '01d')
-        }
-    else:
-        # If the city isn't found, we send back an error message
-        context = {
-            'city': 'Not Found',
-            'temp': '--',
-            'desc': 'Please check the city name.',
-            'icon': '',
-        }
-    # 5. Send that info to the HTML file
+        if response.get('cod') == 200:
+            context = {
+                'city': response['name'],
+                'temp': response['main']['temp'],
+                'desc': response['weather'][0]['description'],
+                'icon': response['weather'][0]['icon'],
+            }
+        else:
+            context = {
+                'city': 'Not Found',
+                'temp': '--',
+                'desc': 'Please check the city name.',
+                'icon': '',
+            }
+
+    # 3. Send the 'context' dictionary to the HTML
     return render(request, 'weather/index.html', context)
